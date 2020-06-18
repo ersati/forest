ad228e9b664155e4735ba1bee426589780d4d287
Piotr Pacewicz <piotrp92@gmail.com>
2020-06-18 20:16:27 +0100

    add function to caunt result

---
+++ index.js
@@ -0,0 +1,44 @@
+const images = {
+  treasure: "💰",
+  leafs: "🍃",
+  trees: "🌲",
+};
+
+const container = document.querySelector(".entry");
+const trees = document.querySelectorAll(".entry .tree");
+const result = document.querySelector(".result");
+const btnClear = document.querySelector(".clear");
+const btnReset = document.querySelector(".reset");
+const tresuare = document.querySelector("div.treasure");
+
+let total = 0;
+
+const updateResult = () => {
+  result.textContent = `Result: ${total}`;
+};
+
+container.addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("treasure")) {
+    e.target.innerHTML = images.treasure;
+    total++;
+    updateResult();
+    alert("you won");
+  } else if (e.target.classList.contains("tree")) {
+    e.target.innerHTML = images.leafs;
+  }
+});
+container.addEventListener("mouseout", (e) => {
+  if (e.target.innerHTML === images.leafs) {
+    e.target.innerHTML = images.trees;
+  }
+});
+
+btnClear.addEventListener("click", () => {
+  tresuare.innerHTML = images.trees;
+});
+
+btnReset.addEventListener("click", () => {
+  tresuare.innerHTML = images.trees;
+  total = 0;
+  updateResult();
+});
